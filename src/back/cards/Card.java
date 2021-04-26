3d195a60b97704fcc6ae564ac6dd2dc56c08c116
RCaby <raphael.caby@imt-atlantique.net>
2021-04-27 00:50:05 +0200

    Debug

--- src/back/cards/Card.java
+++ src/back/cards/Card.java
@@ -29,7 +29,6 @@ public abstract class Card implements Serializable {
 
     public void useCard(Player player1, Player player2, Player player3, ActionType action) {
         board.getCardsPlayedThisRound().add(this);
-
         if (!isRevealed) {
             isRevealed = true;
             if (owner != null) {
--- src/back/cards/StagnantWater.java
+++ src/back/cards/StagnantWater.java
@@ -21,9 +21,8 @@ public class StagnantWater extends Card {
     @Override
     public void useCard(Player player1, Player player2, Player player3, ActionType action) {
         if (player1 != null) {
-            System.out.println("Hey\n\n\n\n");
 
-            useCard(player1, player2, player3, action);
+            super.useCard(player1, player2, player3, action);
             board.addWater(1);
             if (!board.getMatchesUsedThisRound()) {
                 player1.setState(PlayerState.SICK);
